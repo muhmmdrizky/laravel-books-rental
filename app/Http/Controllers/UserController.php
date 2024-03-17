ccbf77e63344d6c5e571a27b78bff4b2874e75a5
Muhammad Rizky <muhmmdrizkymr@gmail.com>
2024-03-17 16:55:19 +0700

    feat: create user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+    public function index()
+    {
+        return view('pages/users/users');
+    }
     public function profile()
     {
         return view('pages/users/profile');
