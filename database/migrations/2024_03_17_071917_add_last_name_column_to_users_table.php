f37b000d2a69198b8738ff841c0e02a69de8397d
Muhammad Rizky <muhmmdrizkymr@gmail.com>
2024-03-17 14:24:04 +0700

    feat: add column last_name to users table

--- database/migrations/2024_03_17_071917_add_last_name_column_to_users_table.php
+++ database/migrations/2024_03_17_071917_add_last_name_column_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('last_name', 100)->nullable(false)->after('first_name');
+            $table->string('last_name', 100)->nullable(true)->after('first_name');
         });
     }
 
